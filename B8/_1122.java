ba6d8c028d4b4ab4c51b5155f98491eeb7ac1a91
Mac Hung <macvanhung_t64@hus.edu.vn>
2022-10-21 00:49:29 +0700

    Update bai tap ve nha buoi 8

--- B8/_1122.java
+++ B8/_1122.java
@@ -3,30 +3,6 @@ package B8;
 import java.util.*;
 
 public class _1122 {
-//    public static int[] relativeSortArray(int[] arr1, int[] arr2) {
-//        Map<Integer, Integer> map = new TreeMap<Integer, Integer>();
-//        int[] result = new int[arr1.length];
-//        for (int nums : arr1) {
-//            if(map.get(nums) == null)
-//                map.put(nums, 1);
-//            else{
-//                map.put(nums, map.get(nums) + 1);
-//            }
-//        }
-//        int count = 0;
-//        for (int nums : arr2) {
-//            for (int i = 0; i < map.get(nums); i++) {
-//                result[count++] = nums;
-//            }
-//            map.remove(nums);
-//        }
-//        for (int nums : map.keySet()) {
-//            for (int i = 0; i < map.get(nums); i++) {
-//                result[count++] = nums;
-//            }
-//        }
-//        return result;
-//    }
     public static int[] relativeSortArray(int[] arr1, int[] arr2) {
         int[] arr = new int[1001];
         int[] result = new int[arr1.length];
